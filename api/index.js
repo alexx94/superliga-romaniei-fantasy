f79d6f6470a9bccf43fdbf781e8464c36c54f39b
Alex Balagiu <alex.balagiu109@gmail.com>
2025-05-04 18:41:06 +0300

    Test Vercel Deploy - v0.1

--- api/index.js
+++ api/index.js
@@ -1,7 +1,8 @@
 import dotenv from 'dotenv';
-import express from 'express';
+import express, { application } from 'express';
 import cookieParser from 'cookie-parser';
 import cors from 'cors';
+import ServerlessHttp from 'serverless-http'; 
 
 //CORS doar daca frontend face request la API de pe un alt domeniu
 const FRONTEND_URL = process.env.FRONTEND_URL;
@@ -19,13 +20,15 @@ app.use(cors(corsOptions));
 
 //TODO: Global error handler for all routes later on
 
-import homeRoute from './src/routes/Home.js';
-import playerRoute from './src/routes/Players.js';
-import authRoute from './src/routes/Auth.js';
+import homeRoute from '../src/routes/Home.js';
+import playerRoute from '../src/routes/Players.js';
+import authRoute from '../src/routes/Auth.js';
 
 app.use('/', homeRoute);
 app.use('/api', playerRoute);
 app.use('/auth', authRoute);
 
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => { console.log(`Server is running on port ${PORT}`); })
+//const PORT = process.env.PORT || 5000;
+//app.listen(PORT, () => { console.log(`Server is running on port ${PORT}`); })
+
+export const handler = ServerlessHttp(app);
