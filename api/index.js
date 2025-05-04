ff6aa802a11a0a25efbfa1779a3430f209e55e74
Alex Balagiu <alex.balagiu109@gmail.com>
2025-05-04 19:01:23 +0300

    Test Vercel Deploy - v0.2

--- api/index.js
+++ api/index.js
@@ -31,4 +31,4 @@ app.use('/auth', authRoute);
 //const PORT = process.env.PORT || 5000;
 //app.listen(PORT, () => { console.log(`Server is running on port ${PORT}`); })
 
-export const handler = ServerlessHttp(app);
+export default ServerlessHttp(app);
