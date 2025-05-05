dbcedd354366a1421131e360be2711977e226ded
Alex Balagiu <alex.balagiu109@gmail.com>
2025-05-05 12:30:15 +0300

    ..

--- api/index.js
+++ api/index.js
@@ -38,7 +38,6 @@ app.use('/{*splat}', (req, res) => {
     res.sendFile(path.join(__dirname, '../client/build/index.html'));
   });
 
-// const PORT = process.env.PORT || 5000;
-// app.listen(PORT, () => { console.log(`Server is running on port ${PORT}`); })
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => { console.log(`Server is running on port ${PORT}`); })
 
-export default app;
